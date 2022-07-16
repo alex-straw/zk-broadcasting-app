ffff1066f094e5f37ce22faf9ca38fb1dca60007
alex-straw <as17163@bristol.ac.uk>
2022-07-16 23:22:04 +0100

    slight changes?

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -9,16 +9,10 @@ contract IdentityFactory is Verifier {
     uint public uploadCount = 0;
     string[] public ipfsCIDs;
     mapping(bytes32 => bool) usedProof;
-    
-    // Add a mapping here containing usedProofs
-
-    function invalidateProof(Proof memory proof) {
-        usedProof[sha256(Proof)] = true;
-    }
 
     function broadcastData(Proof memory proof, string memory cid) public { 
         if (verifyTx(proof) == true) {
-            bytes32 proofId = sha256(Proof);
+            bytes32 proofId = keccak256(abi.encode(proof.a, proof.b, proof.c));
             require(usedProof[proofId] != true, "Proof has already been used");
             usedProof[proofId] = true;
             uploadCount += 1;
