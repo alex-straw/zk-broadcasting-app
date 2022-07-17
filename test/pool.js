ed269180fda2922126bee91bc5af236d7b18a625
alex-straw <as17163@bristol.ac.uk>
2022-07-17 09:54:36 +0100

    added additional comments

--- test/pool.js
+++ test/pool.js
@@ -15,6 +15,8 @@ describe("zk-broadcasting app setup (pool)", function () {
         5. Export Verifier.sol - zk-SNARK contract (using verification.key)
         6. Deploy Pool.sol - which is an extension of the pool specific 'Verifier.sol' file
         7. Using public proving.key file, a user will generate a proof of pre-image
+            a) Might be necessary to use exec commands here as the compiled binaries are much faster
+               than the javascript library - discuss with Theo.
         8. By calling the 'broadcastData' function on the Pool.sol contract they will verify this tx
         9. If the proof is valid, and unused previously, they will append an IPFS CID to the ipfsCIDs array
         10. IPFS data can be accessed by using a or b:
