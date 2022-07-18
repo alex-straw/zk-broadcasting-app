e60ca1ee34fd8fbc0be6580b53c4bbf59dfe8975
alex-straw <as17163@bristol.ac.uk>
2022-07-18 14:51:59 +0100

    changed modules to get hardhat working properly - different hardhat install

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-require("@nomiclabs/hardhat-ethers"); 
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.9",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
+
