f049d1c411a4632fd1291a8690bdaafb67b2a499
alex-straw <as17163@bristol.ac.uk>
2022-07-20 10:09:44 +0100

    put testing code into its own callable function

--- zokratesPool/generateProof/generateProof.js
+++ zokratesPool/generateProof/generateProof.js
@@ -32,16 +32,17 @@ async function generateProofOfPreImage(preImage, hashDigest, provingKeyPath, pro
     writeFile(JSON.stringify(proof, null, 2), proofName);
 }   
 
+async function generateTestMemberProof() {
+    /* 
+    This function is used only for testing proof generation with one of the 
+    randomly generated passwords.
+    */ 
 
-/* 
-This would be done client side in future - this is purely for testing purpsoes
-*/ 
+    members = JSON.parse(fs.readFileSync("../setupPasswords/memberPasswords.json"))
+    preImage = members["member_1"].preImage
+    hashDigest = members["member_1"].proofInputDec
+    const provingKeyPath = "../setupPool/proving.key"
+    const proofName = 'testProof.json'
 
-members = JSON.parse(fs.readFileSync("../setupPasswords/memberPasswords.json"))
-preImage = members["member_1"].preImage
-hashDigest = members["member_1"].proofInputDec
-const provingKeyPath = "../setupPool/proving.key"
-const proofName = 'testProof.json'
-
-generateProofOfPreImage(preImage, hashDigest, provingKeyPath, proofName);
-  
+    generateProofOfPreImage(preImage, hashDigest, provingKeyPath, proofName);
+}
