0debecb4d27fef82a7a61497934caf3f15dba148
alex-straw <as17163@bristol.ac.uk>
2022-07-20 10:21:12 +0100

    added function that generates all user test proofs - for 3 members - and a proof for the pool password - for hardhat testing

--- zokratesPool/generateProof/generateProof.js
+++ zokratesPool/generateProof/generateProof.js
@@ -3,18 +3,18 @@ const { initialize } = require('zokrates-js')
 
 function writeFile(data, _fileName) {
     fs.writeFile(_fileName, data, function(err) {
-        if (err) return console.log(err);
-    });
+        if (err) return console.log(err)
+    })
 }
 
 async function generateProofOfPreImage(preImage, hashDigest, provingKeyPath, proofName) {
-    const zokratesProvider = await initialize();
+    const zokratesProvider = await initialize()
     const source = fs.readFileSync("../setupPool/verifyPreImage.zok").toString();
 
     console.log("compilation")
-    const artifacts = zokratesProvider.compile(source);
+    const artifacts = zokratesProvider.compile(source)
 
-    const provingKey = fs.readFileSync(provingKeyPath);
+    const provingKey = fs.readFileSync(provingKeyPath)
 
     console.log("computation")
     const { witness, output } = zokratesProvider.computeWitness(artifacts, [
@@ -24,12 +24,12 @@ async function generateProofOfPreImage(preImage, hashDigest, provingKeyPath, pro
         preImage[3],
         hashDigest[0],
         hashDigest[1]
-    ]);
+    ])
 
     console.log("generate proof")
-    const proof = zokratesProvider.generateProof(artifacts.program, witness, provingKey);
+    const proof = zokratesProvider.generateProof(artifacts.program, witness, provingKey)
 
-    writeFile(JSON.stringify(proof, null, 2), proofName);
+    writeFile(JSON.stringify(proof, null, 2), proofName)
 }   
 
 async function generateTestMemberProof(memberId) {
@@ -54,6 +54,14 @@ async function generatePoolPasswordProof() {
     const provingKeyPath = "../setupPool/proving.key"
     const proofName = 'poolPasswordProof.json'
 
-    generateProofOfPreImage(preImage, hashDigest, provingKeyPath, proofName);
+    generateProofOfPreImage(preImage, hashDigest, provingKeyPath, proofName)
 }
 
+async function generateAllTestProofs() {
+    for (let id=1; i <= 3; i++) {
+        await generateTestMemberProof(`member${id}`)
+    }
+    generatePoolPasswordProof()
+}
+
+generateAllTestProofs()
