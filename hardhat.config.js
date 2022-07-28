20ce5b0e9b4ebf0c6f9a200ed2833eea415b5b3d
alex-straw <as17163@bristol.ac.uk>
2022-07-28 15:32:14 +0100

    Infura API keys inside the .env file. New dependency - dotenv. Made default deploy to Kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,45 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const RINKEBY_API_KEY = process.env.RINKEBY_API_KEY;
+const KOVAN_API_KEY = process.env.KOVAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "kovan",
+  networks: {
+    hardhat: {},
+    kovan: {
+      url: KOVAN_API_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+    rinkeby: {
+      url: RINKEBY_API_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    compilers: [
+      { version: "0.8.0" },
+      { version: "0.8.7" },
+      { version: "0.6.6" },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
-  }
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 3000000,
+  },
 };
-
