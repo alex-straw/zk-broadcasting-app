06be094373581846d84d377b18810ded42427146
alex-straw <as17163@bristol.ac.uk>
2022-08-01 14:50:32 +0100

    renamed for clarity - no longer a listener

--- aws-create-pool/createPool.js
+++ aws-create-pool/createPool.js
@@ -16,11 +16,9 @@ async function main(event) {
 
     filterByName = contract.filters.PoolRequest(event['poolName'], null);
 
-    // events = await contract.queryFilter(filterName);
-
     let pastEvent = await contract.queryFilter(filterByName);
 
-    info = {
+    eventInfo = {
         'hashName': pastEvent[0].args[0].hash,
         'from': pastEvent[0].args[1],
         'idCount': pastEvent[0].args[2],
@@ -29,17 +27,13 @@ async function main(event) {
 
     console.log(info)
     
-    gasPrice = await provider.getGasPrice()
+    const deploymentEstimation = await contract.estimateGas.transfer();
 
+    gasPrice = await provider.getGasPrice()
+    
     console.log(gasPrice)
     return
 }
-
-const emails = [
-    "a@b.bristol.ac.uk",
-    "b@c.bristol.ac.uk",
-    "c@d.bristol.ac.uk"
-]
 
 const event = {
     "emails": {
